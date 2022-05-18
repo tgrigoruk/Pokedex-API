42be04e39bf96a0b9b0ecd0a2b359091d7863306
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-05-17 17:34:27 -0700

    FIxed bug in search (check for empty array)

--- server.js
+++ server.js
@@ -17,15 +17,26 @@ app.get("/pokemon/:id", function (req, res) {
   pokemon = pokemonDB.pokemon.filter((p) => {
     return p.id == req.params.id || p.name == req.params.id;
   })[0];
-  res.send(pokemon);
+  // console.log("requesting: " + pokemon.name);
+  pokemon ? res.send(pokemon) : res.send(false);
 });
 
-app.get("/type", function (req, res) {
-  res.send(pokemonDB.type);
-});
-app.get("/ability", function (req, res) {
-  res.send(pokemonDB.ability);
-});
-app.get("/pokemon-color", function (req, res) {
-  res.send(pokemonDB["pokemon-color"]);
+app.get("/search/:searchType/:searchParam", function (req, res) {
+  let searchType = req.params.searchType;
+  let searchParam = req.params.searchParam;
+
+  if (searchParam === "all") {
+    res.send(pokemonDB[searchType]);
+  } else {
+    searchTypes = searchType == "type" ? "types" : "abilities";
+    matchingPokemon = pokemonDB.pokemon.filter((pokemon) => {
+      let plist = false;
+      pokemon[searchTypes].forEach((searchTypeList) => {
+        // console.log(searchTypeList[searchType].name);
+        if (searchTypeList[searchType].name == searchParam) plist = true;
+      });
+      return plist;
+    });
+    res.send(matchingPokemon);
+  }
 });
